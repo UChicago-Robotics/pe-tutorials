d2fd705913c029f7f1c25cbd4bc63c32ef6c4386
Winston Hartnett <winstonhartnett@gmail.com>
2021-11-17 00:06:42 -0600

    Wallstopper python works

--- src/wallstopper/setup.py
+++ src/wallstopper/setup.py
@@ -1,7 +1,30 @@
 from setuptools import setup
+from pathlib import Path
+from glob import glob
+from collections import defaultdict
+import os
+import fnmatch
 
 package_name = 'wallstopper'
 
+# This section just makes sure our gazebo resources are imported properly.
+wildcard_matches = ['*.model', '*.world', '*.config', '*.sdf', '*.dae']
+folders_dict = defaultdict(list)
+
+for root, dirnames, filenames in os.walk('.'):
+    for wildcard in wildcard_matches:
+        for filename in fnmatch.filter(filenames, wildcard):
+            target_path = os.path.join(root, filename)
+            folders_dict['share/' 
+                 + package_name 
+                 + '/' 
+                 + '/'.join(target_path.split('/')[0:-1])].append(target_path)
+
+matches_out = list(folders_dict.items())
+
+"""
+Configuration
+"""
 setup(
     name=package_name,
     version='0.0.0',
@@ -9,8 +32,8 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+        ('share/' + package_name, ['package.xml', 'launch/wallstopper_world.launch.py']),
+    ] + matches_out,
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='winston',
